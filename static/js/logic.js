a9c3334de65701851f8a1ebb954da180b2891c0a
hekutoru-dev <hectorh.padilla@gmail.com>
2021-02-02 13:59:45 -0600

    Load JSON data from web site

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,3 +9,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(mymap);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(data => {
+    console.log(data);
+
+});
